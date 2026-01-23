0de8e26291d9117aba98f052a66bd537d7c5b144
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-01-23 23:43:05 +0000

    contador simple

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,33 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const SecondsCounter = () => {
-	const [seconds, setSeconds] = useState(0);
+class SecondsCounter extends React.Component {
+	constructor(props) {
+		super(props);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setSeconds(prev => prev + 1);
+		this.state = {
+			seconds: 0
+		};
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			this.setState(prevState => ({
+				seconds: prevState.seconds + 1
+			}));
 		}, 1000);
+	}
 
-		return () => clearInterval(interval);
-	}, []);
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
 
-	const secondsString = seconds.toString().padStart(6, "0");
+	render() {
+		const secondsString = this.state.seconds
+			.toString()
+			.padStart(6, "0");
 
-	return (
-		<div className="container-contador">
-			<div className="contador">
-				<div className="icon">
-					<span className="material-symbols-outlined">schedule</span>
-				</div>
-
-				{secondsString.split("").map((digit, index) => (
-					<div className="num" key={index}>
-						<p>{digit}</p>
+		return (
+			<div className="container-contador">
+				<div className="contador">
+					<div className="icon">
+						<span className="material-symbols-outlined">
+							schedule
+						</span>
 					</div>
-				))}
+
+					{secondsString.split("").map((digit, index) => (
+						<div className="num" key={index}>
+							<p>{digit}</p>
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default SecondsCounter;
